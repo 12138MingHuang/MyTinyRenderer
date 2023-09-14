1d729d23d960a4cf6f4018bdad3937d2d99362cb
张斌 <bin.zhang@appshahe.com>
2023-09-14 17:20:21 +0800

    feat：添加向量工具类操作

--- MyTinyRenderer/geometry.h
+++ MyTinyRenderer/geometry.h
@@ -20,9 +20,12 @@ public:
 	inline Vec2<t> operator +(const Vec2<t>& V) const { return Vec2<t>(x + V.x, y + V.y); }//向量加法
 	inline Vec2<t> operator -(const Vec2<t>& V) const { return Vec2<t>(x - V.x, y - V.y); }//向量减法
 	inline Vec2<t> operator *(float f)          const { return Vec2<t>(x * f, y * f); }//向量的数乘运算
+	inline Vec2<t> operator/(const t& V) const { return Vec2<t>(x / V, y / V); }// 向量与标量的除法
+	inline t operator *(const Vec2<t>& V) const { return x * V.x + y * V.y; } // 点乘运算
 
 	inline t& operator[](const int idx) { if (idx <= 0) return x; else return y; }
 
+	inline Vec2<t> cwiseProduct(const Vec2<t>& V) const { return Vec2<t>(x * V.x, y * V.y); } // 逐元素乘法
 	float norm() const { return std::sqrt(x * x + y * y); }//向量的模长
 	Vec2<t>& normalize(t l = 1) { *this = (*this) * (l / norm()); return *this; }//向量归一化
 
@@ -59,14 +62,17 @@ struct Vec3 {
 	inline Vec3<t> operator +(const Vec3<t>& V) const { return Vec3<t>(x + V.x, y + V.y, z + V.z); }//向量加法
 	inline Vec3<t> operator -(const Vec3<t>& V) const { return Vec3<t>(x - V.x, y - V.y, z - V.z); }//向量减法
 	inline Vec3<t> operator *(float f)          const { return Vec3<t>(x * f, y * f, z * f); }//向量的数乘运算
+	inline Vec3<t> operator/(const t& V) const { return Vec3<t>(x / V, y / V, z / V); }// 向量与标量的除法
 	inline Vec3<t> operator ^(const Vec3<t>& v) const { return Vec3<t>(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x); }//向量叉乘运算（外积）
-	inline t       operator *(const Vec3<t>& v) const { return x * v.x + y * v.y + z * v.z; }//向量的点乘（内积）
+	inline t       operator *(const Vec3<t>& v) const { return x * v.x + y * v.y + z * v.z; }//向量的点乘（内积)
 
+	inline t& operator[](const int idx) { if (idx <= 0) return x; else if (idx == 1) return y; else return z; }
+
+	inline Vec3<t> cwiseProduct(const Vec3<t>& v) const { return Vec3<t>(x * v.x, y * v.y, z * v.z); }//逐元素乘法运算
 	float norm() const { return std::sqrt(x * x + y * y + z * z); }//向量的模长
 	Vec3<t>& normalize(t l = 1) { *this = (*this) * (l / norm()); return *this; }//向量归一化
 
 	template <class > friend std::ostream& operator<<(std::ostream& s, Vec3<t>& v);//向量输出
-	inline t& operator[](const int idx) { if (idx <= 0) return x; else if (idx == 1) return y; else return z; }
 };
 
 template <class t>
@@ -101,13 +107,16 @@ struct Vec4
 	inline Vec4<t> operator +(const Vec4<t>& V) const { return Vec4<t>(x + V.x, y + V.y, z + V.z, w + V.w); }//向量加法
 	inline Vec4<t> operator -(const Vec4<t>& V) const { return Vec4<t>(x - V.x, y - V.y, z - V.z, w - V.w); }//向量减法
 	inline Vec4<t> operator *(float f)          const { return Vec4<t>(x * f, y * f, z * f, w * f); }//向量的数乘运算
+	inline Vec4<t> operator/(const t& V) const { return Vec4<t>(x / V, y / V, z / V ,w / V); }// 向量与标量的除法
 	inline t       operator *(const Vec4<t>& v) const { return x * v.x + y * v.y + z * v.z + w * v.w; }//向量的点乘（内积）
 
+	inline t& operator[](const int idx) { if (idx <= 0) return x; else if (idx == 1) return y; else if (idx == 2) return z; else return w; }
+
+	inline Vec4<t> cwiseProduct(const Vec4<t>& v) const { return Vec4<t>(x * v.x, y * v.y, z * v.z, w * v.w); }//逐元素乘法
 	float norm() const { return std::sqrt(x * x + y * y + z * z + w * w); }//向量的模长
 	Vec4<t>& normalize(t l = 1) { *this = (*this) * (l / norm()); return *this; }//向量归一化
 
 	template <class > friend std::ostream& operator<<(std::ostream& s, Vec4<t>& v);//向量输出
-	inline t& operator[](const int idx) { if (idx <= 0) return x; else if (idx == 1) return y; else if (idx == 2) return z; else return w; }
 };
 
 template <class t>
@@ -115,6 +124,8 @@ std::ostream& operator<<(std::ostream& s, Vec4<t>& v) {
 	s << "(" << v.x << ", " << v.y << ", " << v.z << ", " << v.w << ")\n";
 	return s;
 }
+
+
 
 //模板特化,并且重命名
 typedef Vec4<float> Vec4f;
